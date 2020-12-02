e44e8a63890dc926dbafc794e946462347d91136
Mike Frenil <lmfrenil@gmail.com>
2020-12-01 22:40:09 -0800

    Moving models from  vendor

---
+++ lib/models/lib/catalog.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/foundation.dart';
+
+class CatalogItem {
+  final String name;
+  final int id;
+  final Unit unitOfMeasure;
+  final int categoryId;
+  final String categoryName;
+  final int subCategoryId;
+  final String subCategoryName;
+  final double price;
+  final List<String> tags;
+
+  CatalogItem({
+    this.name,
+    @required this.id,
+    this.unitOfMeasure = Unit.pieces,
+    this.categoryId,
+    this.categoryName,
+    this.subCategoryId,
+    this.subCategoryName,
+    this.price,
+    this.tags = const [],
+  });
+
+  CatalogItem.fromJson(Map<String, dynamic> json)
+      : this(
+          id: json['itemId'],
+          name: json['itemName'],
+          categoryId: json['categoryId'],
+          categoryName: json['categoryName'],
+          subCategoryId: json['subCategoryId'],
+          subCategoryName: json['subCategoryName'],
+          price: (json['price'] as int).toDouble(),
+          unitOfMeasure: asUnit(json['unitOfMeasure']),
+          tags: json['tags'] ?? [],
+        );
+}
+
+enum Unit {
+  kg,
+  pieces,
+}
+
+extension UnitExtension on Unit {
+  String asString() {
+    switch (this) {
+      case Unit.kg:
+        return 'kg';
+        break;
+      case Unit.pieces:
+        return 'pc';
+        break;
+      default:
+        throw UnsupportedError('Unsupported unit.');
+    }
+  }
+
+  double get incrementValue {
+    switch (this) {
+      case Unit.kg:
+        return 0.25;
+        break;
+      case Unit.pieces:
+        return 1;
+        break;
+      default:
+        throw UnsupportedError('Unsupported unit.');
+    }
+  }
+}
+
+Unit asUnit(String value) {
+  return Unit.values
+      .firstWhere((e) => e.toString() == 'Unit.${value.toLowerCase()}');
+}
---
+++ lib/models/lib/order.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/foundation.dart';
+import 'package:strings/strings.dart';
+import 'package:vendor/models/lib/catalog.dart';
+
+class Order {
+  final int orderId;
+  final int customerId;
+  final DateTime orderDate;
+  final double totalPrice;
+  OrderStatusEnum orderStatus;
+  final bool isDelivery;
+  final String customerName;
+
+  Order({
+    @required this.orderId,
+    @required this.customerId,
+    @required this.orderDate,
+    @required this.totalPrice,
+    @required this.orderStatus,
+    @required this.isDelivery,
+    @required this.customerName,
+  });
+
+  Order.fromJson(Map<String, dynamic> json)
+      : this(
+          customerId: json['customerId'],
+          orderId: json['orderId'],
+          orderDate: DateTime.parse(json['orderDate']),
+          orderStatus: toOrderStatusEnum(json['orderStatus']),
+          isDelivery: json['isDeliver'],
+          totalPrice: json['totalPrice'].toDouble(),
+          customerName: json['customerName'],
+        );
+}
+
+enum OrderStatusEnum {
+  pending,
+  accepted,
+  rejected,
+  completed, // delivered or picked up.
+  outToDeliver,
+  readyToPick,
+}
+
+extension OrderStatusEnumExtension on OrderStatusEnum {
+  String asString() {
+    return describeEnum(this)
+        .replaceAllMapped(RegExp("([A-Z])"), (m) => ' ${m[0].toLowerCase()}');
+  }
+
+  String asActionString() {
+    switch (this) {
+      case OrderStatusEnum.pending:
+        return 'Pending';
+      case OrderStatusEnum.accepted:
+        return 'Accept';
+      case OrderStatusEnum.rejected:
+        return 'Reject';
+      case OrderStatusEnum.completed:
+        return 'Complete';
+      case OrderStatusEnum.outToDeliver:
+        return 'Out to deliver';
+      case OrderStatusEnum.readyToPick:
+        return 'Ready to pick';
+      default:
+        throw 'Invalid Status';
+    }
+  }
+}
+
+OrderStatusEnum toOrderStatusEnum(String value) {
+  return OrderStatusEnum.values.firstWhere((e) =>
+      e.toString().toLowerCase() == 'orderstatusenum.${value.toLowerCase()}');
+}
+
+class ItemOrder {
+  final int quantity;
+  final CatalogItem item;
+  final double subTotalPrice;
+
+  ItemOrder({
+    @required this.item,
+    @required this.quantity,
+    @required this.subTotalPrice,
+  });
+
+  ItemOrder.fromJson(Map<String, dynamic> json)
+      : this(
+          quantity: json['quantity'],
+          subTotalPrice: json[''],
+          item: CatalogItem(id: json['itemId']),
+        );
+
+  Map<String, dynamic> toMap() => {
+        'itemId': item.id,
+        'quantity': quantity,
+        'subTotalPrice': subTotalPrice,
+      };
+}
+
+class OrderStatus {
+  final int orderId;
+  final OrderStatusEnum status;
+
+  OrderStatus({@required this.orderId, @required this.status});
+
+  static OrderStatus fromJson(Map<String, dynamic> json) => OrderStatus(
+        orderId: json['orderId'],
+        status: toOrderStatusEnum(json['orderStatus']),
+      );
+
+  Map<String, dynamic> toMap() => {
+        'orderId': orderId,
+        'orderStatus': capitalize(describeEnum(status)),
+      };
+}
